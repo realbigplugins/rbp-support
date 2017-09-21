12fe8038de7da29a7c11421bff3ddbd81c646294
Eric Defore <eric.defore@gmail.com>
2017-09-21 16:47:35 -0400

    init commit. nothing yet

---
+++ rbp-support.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Class RBP_Support
+ *
+ * Allows a Support Form to be quickly added to our Plugins
+ * It includes a bunch of (filterable) Debug Info that gets sent along with the Email
+ *
+ * @since {{VERSION}}
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'RBP_Support' ) ) {
+	
+	class RBP_Support() {
+		
+		/**
+		 * RBP_Support constructor.
+		 * 
+		 * @since		{{VERSION}}
+		 */
+		function __construct() {
+		}
+		
+	}
+	
+}
