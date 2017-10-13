baf43ebb2990aaeed0a284efb169c37e5a1598d2
Eric Defore <eric.defore@gmail.com>
2017-10-13 10:13:04 -0400

    Disable Form Submit to prevent rapid fire submissions

--- build/js/admin/form/submit.js
+++ build/js/admin/form/submit.js
@@ -2,6 +2,19 @@
 	
 	$( document ).ready( function() {
 		
+		if ( $( '.rbp-support-form' ).length <= 0 ) return;
+		
+		// Only apply to <form> variant
+		$( 'form.rbp-support-form' ).on( 'submit', function( event ) {
+			
+			// For some dumb reason, if we use document.activeElement here it prevents the form submission
+			// Not in the JavaScript validation version though
+			var $submitButton = $( this ).find( 'button[type="submit"]' );
+			
+			$submitButton.attr( 'disabled', true );
+			
+		} );
+		
 		if ( $( '.rbp-support-form.javascript-interrupt' ).length <= 0 ) return;
 		
 		$( 'form' ).on( 'submit', function( event ) {
@@ -11,6 +24,8 @@
 			// Check to see if it is our Submit Button
 			// A lot of our plugins tie into other systems (EDD, PSP, etc.) which often means we're creating something inside of another <form> with little options to place outside of it
 			if ( $submitButton.attr( 'name' ).indexOf( '_support_submit' ) > -1 ) {
+				
+				$submitButton.attr( 'disabled', true );
 				
 				var $form = $( this );
 				
@@ -35,6 +50,9 @@
 						$form.find( '.required' ).each( function( index, element ) {
 							$( element ).attr( 'required', false );
 						} );
+						
+						// Reset here to line up with the rest of the form "reset"
+						$submitButton.attr( 'disabled', false );
 
 					}, 2000 );
 					
